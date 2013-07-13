3297ba3e1090f222a8bfe18e99aa90991329cd39
Fei Wang <wangfei001@hotmail.com>
2013-07-13 22:45:35 +1000

    login/logout

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -17,17 +17,22 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
+
+        $user = User::model()->find('username = :username',array('username'=>$this->username));
+
+        if($user){
+            if($user->password != $this->password){
+                $this->errorCode=self::ERROR_PASSWORD_INVALID;
+            }else{
+                $this->errorCode=self::ERROR_NONE;
+                return true;
+            }
+        }else{
+            $this->errorCode=self::ERROR_USERNAME_INVALID;
+        }
+
+
+        return false;
+
 	}
 }
--- protected/controllers/AccountController.php
+++ protected/controllers/AccountController.php
@@ -58,4 +58,13 @@ class AccountController extends Controller
 
         $this->render('create', array('model' => $model));
     }
+
+    /**
+     * Logs out the current user and redirect to homepage.
+     */
+    public function actionLogout()
+    {
+        Yii::app()->user->logout();
+        $this->redirect(Yii::app()->homeUrl);
+    }
 }
